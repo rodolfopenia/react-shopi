c89f2a46599890d63c3d412f7a687207e1e0ab34
Rodolfo Peña <rodolfo.penia@hotmail.com>
2024-01-03 23:40:30 -0500

    Add ProductDetail logic

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,12 +1,16 @@
+import { useContext } from 'react'
+import { ShoppingCartContext } from '../../Context'
 import './styles.css'
 import { XMarkIcon } from '@heroicons/react/24/solid'
 
 const ProductDetail = () => {
+  const context = useContext(ShoppingCartContext)
   return (
-    <aside className='product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white'>
+    <aside className={`${context.isProductDetailOpen ? 'flex' : 'hidden'} product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white`}>
       <div className='flex justify-between items-center p-6'>
         <h2 className='font-medium text-xl'>Detail</h2>
-        <div>
+        <div className='cursor-pointer'
+          onClick={() => context.closeProductDetail()}>
           <XMarkIcon className='h-6 w-6' />
         </div>
       </div>
