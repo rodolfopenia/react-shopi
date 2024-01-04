624caa729c87efd2bda474b0352f882956851174
Rodolfo Peña <rodolfo.penia@hotmail.com>
2024-01-03 22:29:00 -0500

    Add Product Detail Component and Icons

---
+++ src/Components/ProductDetail/index.jsx
@@ -0,0 +1,17 @@
+import './styles.css'
+import { XMarkIcon } from '@heroicons/react/24/solid'
+
+const ProductDetail = () => {
+  return (
+    <aside className='product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white'>
+      <div className='flex justify-between items-center p-6'>
+        <h2 className='font-medium text-xl'>Detail</h2>
+        <div>
+          <XMarkIcon className='h-6 w-6' />
+        </div>
+      </div>
+    </aside>
+  )
+}
+
+export default ProductDetail
