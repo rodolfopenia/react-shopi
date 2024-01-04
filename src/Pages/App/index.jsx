760eff3829001ce1b86821359976bfc7fce101ab
Rodolfo Peña <rodolfo.penia@hotmail.com>
2024-01-03 21:30:42 -0500

    Add global context

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,4 +1,5 @@
 import { useRoutes, BrowserRouter } from 'react-router-dom'
+import { ShoppingCartProvider } from '../../Context'
 import Home from '../Home'
 import MyAccount from '../MyAccount';
 import MyOrder from '../MyOrder';
@@ -22,14 +23,14 @@ const AppRoutes = () => {
 }
 
 function App() {
-
-
   return (
     <>
+    <ShoppingCartProvider>
       <BrowserRouter>
         <AppRoutes />
         <Navbar />
       </BrowserRouter>
+    </ShoppingCartProvider>
     </>
   )
 }
