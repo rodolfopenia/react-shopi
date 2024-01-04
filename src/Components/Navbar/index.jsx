7cb47bb2a59cb307ae441ce66960a7858107a8bd
Rodolfo Peña <rodolfo.penia@hotmail.com>
2024-01-03 21:57:15 -0500

    Use Global Context Count

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,6 +1,9 @@
+import { useContext } from 'react'
+import { ShoppingCartContext } from '../../Context'
 import { NavLink } from "react-router-dom"
 
 const Navbar = () => {
+  const context = useContext(ShoppingCartContext)
   const activeStyle = 'underline underline-offset-4'
 
   return (
@@ -90,7 +93,7 @@ const Navbar = () => {
           </NavLink>
         </li>
         <li>
-          🛒 0
+          🛒 {context.count}
         </li>
       </ul>
     </nav>
