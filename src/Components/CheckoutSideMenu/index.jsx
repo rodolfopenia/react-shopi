cb36a4d1695afb63d02e7912bdd35929f22de514
Rodolfo Peña <rodolfo.penia@hotmail.com>
2024-01-05 10:42:57 -0500

    Add OrderCard Component

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import { ShoppingCartContext } from '../../Context'
 import { XMarkIcon } from '@heroicons/react/24/solid'
+import OrderCard from '../OrderCard'
 import './styles.css'
 
 const CheckoutSideMenu = () => {
@@ -14,6 +15,19 @@ const CheckoutSideMenu = () => {
           <XMarkIcon className='h-6 w-6' />
         </div>
       </div>
+      <div className='px-6'>
+        {
+          context.cartProducts.map(product => (
+            <OrderCard 
+              key={product.id}
+              title={product.title} 
+              imageUrl={product.images}
+              price={product.price}
+              />
+          ))
+        }
+      </div>
+      
     </aside>
   )
 }
