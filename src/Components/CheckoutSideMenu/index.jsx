d6ea24816767054337fdebc2865e2fc110939143
Rodolfo Peña <rodolfo.penia@hotmail.com>
2024-03-19 00:28:03 -0500

    Add myOrders button

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -13,6 +13,18 @@ const CheckoutSideMenu = () => {
     context.setCartProducts(filteredProducts)
   }
 
+  const handleCheckout = () => {
+    const orderToAdd = {
+      date: '01.02.23',
+      products: context.cartProducts,
+      totalProducts: context.cartProducts.length,
+      totalPrice: totalPrice(context.cartProducts)
+    }
+
+    context.setOrder([...context.order, orderToAdd])
+    context.setCartProducts([])
+  }
+
   return (
     <aside className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex flex-col fixed right-0 border border-black rounded-lg bg-white`}>
       <div className='flex justify-between items-center p-6'>
@@ -22,7 +34,7 @@ const CheckoutSideMenu = () => {
           <XMarkIcon className='h-6 w-6' />
         </div>
       </div>
-      <div className='px-6 overflow-y-auto'>
+      <div className='px-6 overflow-y-scroll flex-1'>
         {
           context.cartProducts.map(product => (
             <OrderCard 
@@ -36,11 +48,13 @@ const CheckoutSideMenu = () => {
           ))
         }
       </div>
-      <div className='px-6'>
-        <p className='flex justify-between items-center'>
+      <div className='px-6 mb-6'>
+        <p className='flex justify-between items-center mb-2'>
           <span className='font-light'>Total:</span>
           <span className='font-medium text-2xl'>${totalPrice(context.cartProducts)}</span>
         </p>
+        <button className='bg-black py-3 text-white w-full rounded'
+        onClick={() => handleCheckout()}>Checkout</button>
       </div>
     </aside>
   )
